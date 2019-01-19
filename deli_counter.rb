de9f66490bb24047a4ce0dcc34ea0cf1d7a36281
H B <phajib@gmail.com>
2019-01-19 07:38:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
+    #puts "The line is currently: "
     array.map do |people|
       puts "#{x}. #{people}"
       x+=1
