3f9f1693cfba9d994791f1918c5c79c1fa07df10
H B <phajib@gmail.com>
2019-01-19 07:29:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,13 @@ require 'benchmark'
 puts Benchmark.measure { 10_000_000.times { Object.new } }
 
 def line(array)
-  x = 0
+  x = 1
   if array.length == 0
     puts "The line is currently empty."
   else
-    array.map do |people| x+=1
+    array.map do |people|
       puts "The line is currently: #{x}. #{people}"
+      x+=1
     end
   end
 end
