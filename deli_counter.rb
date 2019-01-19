1b6f8ddfda65871a1decf429841918b2a29ba116
H B <phajib@gmail.com>
2019-01-19 07:19:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
   else
     array.map do |people|
       x+=1
-      "The line is currently: #{x}. #{people}"
+      puts "The line is currently: #{x}. #{people}"
     end
   end
 end
